40250c82b704c78d986cec228024b3b9ef2531f2
unknown <dhanushgdckap@gmail.com>
2023-09-07 12:13:29 +0530

    started the Todo for the basic learning.

--- hello-world/App.js
+++ hello-world/App.js
@@ -1,40 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{ useState } from 'react';
 import { StyleSheet, Text, View , FlatList,TouchableOpacity} from 'react-native';
+import Header from './Componets/header';
 
 export default function App() {
-  const [people, setPeople] = useState([
-    {name : 'dhanush',id: '1'},
-    {name : 'Jeeva',id: '2'},
-    {name : 'Kishore',id: '3'},
-    {name : 'Parasuraman',id: '4'},
-    {name : 'Deepak',id: '5'},
-    {name : 'Akash',id: '6'},
-    {name : 'Sunil',id: '7'},
-    {name : 'Bharat',id: '8'},
 
+  const [todos,setTodos] = useState([
+    {text: 'buy coffee',key:'1'},
+    {text: 'create an app',key:'2'},
+    {text: 'play on the switch',key:'3'},
   ])
 
-  const pressHandler = (id) => {
-        console.log(id);
-        setPeople((prevPeople) => {
-          return prevPeople.filter(person => person.id != id)
-        })
-  }
   return (
     <View style={styles.container}>
-      <FlatList 
-      numColumns={2}
-      keyExtractor={(item) => item.id}
-        data={people}
-        renderItem={({item})=>(
-          <TouchableOpacity onPress={() => {
-            pressHandler(item.id)
-          }}>
-            <Text style={styles.listItem}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
+      {/*header*/}
+      <Header />
+      <View style={styles.content}>
+        {/* for form */}
+        <View style={styles.list}>
+          <FlatList
+            data = {todos}
+            renderItem={({item}) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+
+        </View>
+      </View>
     </View>
   );
 } 
@@ -43,17 +35,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop:40,
-    paddingHorizontal:20,
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
-  listItem:{
-    marginTop:24,
-    padding:30,
-    backgroundColor:'pink',
-    fontSize:24,
-    marginHorizontal:10,
-    marginTop:24,
+  content:{
+    padding:40,
+  },
+  list:{
+    marginTop:20
   }
 });
